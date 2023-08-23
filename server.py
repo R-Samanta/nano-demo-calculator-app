8fcbf99c9b42ba7bae7fc76cf2e8500d63415695
R-Samanta <rudrarudra2015@outlook.com>
2023-08-24 02:07:11 +0530

    commit1

--- server.py
+++ server.py
@@ -16,6 +16,7 @@ def greeting():
 @app.route("/calculator/add", methods=['POST'])
 def add():
     numbers = request.json
+    print('here')
     response = Result(numbers['first'] + numbers['second'])
     return jsonify(response)
 
