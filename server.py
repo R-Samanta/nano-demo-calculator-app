33b988c6b789a7fe5fee1b36ac1e38aaf055ec27
R-Samanta <rudrarudra2015@outlook.com>
2023-08-23 23:50:30 +0530

    Revert "Remove actual answers"
    This reverts commit 0084e0fffff490c1745b3c02e71a78eed88181ed.

--- server.py
+++ server.py
@@ -1,4 +1,10 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from dataclasses import dataclass
+
+@dataclass
+class Result:
+    result: int
+
 
 app = Flask(__name__)
 
@@ -9,11 +15,15 @@ def greeting():
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    numbers = request.json
+    response = Result(numbers['first'] + numbers['second'])
+    return jsonify(response)
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    numbers = request.json
+    response = Result(numbers['first'] - numbers['second'])
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
